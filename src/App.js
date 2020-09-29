14d1591b2928b3254b3cca802bf35c1eb7ad5279
akozlova <annakozlova92@gmail.com>
2020-09-28 22:09:31 -0700

    add styles to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , { useState, useEffect } from 'react';
 import * as BooksAPI from './BooksAPI'
 import AllBooks from './components/AllBooks';
+import './App.css'
 
 export default function App () {
 
